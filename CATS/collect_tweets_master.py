c9bb058bb8fb69b948864310027f8b402ae4a0eb
Janet van Bilsen <janet.vanbilsen@outlook.com>
2020-11-09 09:24:07 +0000

    Update

--- CATS/collect_tweets_master.py
+++ CATS/collect_tweets_master.py
@@ -9,10 +9,10 @@ import sys
 from csv import writer
 
 # Twitter API keys
-consumer_key = 'uEgngDaydm9A5upXp44j6L2XM'
-consumer_secret = 'lCMlwFbT0d4FTElh33wuay1NCHNC5W9b49regjGH7RJ40BXTer'
-access_token = '1306187660592123905-KHAcQ4Cyiw5IcB64Tys95Hb03sSYbe'
-secret_token_secret = 'c4qxlhQsjsf02l4CrEJfzPcdHIReHZYgHmqrkUnP1QXct'
+consumer_key = '*************************'
+consumer_secret = '*************************************************'
+access_token = '*******************-******************************'
+secret_token_secret = '*********************************************'
 
 # Gaining Twitter access
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -20,15 +20,15 @@ auth.set_access_token(access_token, secret_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 # Places that should be included in geographical location
-uk_places = pd.read_excel('/home/pi/Sync/twitter/Places_UK.xlsx')
-uk_places = list(uk_places.Place)
-uk_places = [place.lower() for place in uk_places]
+incl_places = pd.read_excel('pathname for incl_places.xlsx file')
+incl_places = list(incl_places.place)
+incl_places = [place.lower() for place in uk_places]
 
 # Places that are to be filtered out from geographical location
-no_places = pd.read_excel('/home/pi/Sync/twitter/Location_exceptions.xlsx').Place.to_list()
+excl_places = pd.read_excel('pathname for excl_place.xlsx').Place.to_list()
 
 # Keywords that will be used to search Twitter
-keywords = ['depression', 'depressed', 'depressive', 'depressing']
+keywords = ['keyword1', 'keyword2', 'keyword3']
 
 # Current date to label the file name
 current_date = time.strftime('%Y%m%d')
@@ -55,14 +55,14 @@ class MyStreamListener(StreamListener):
             user_data = []
             
             # Opens time tweets extracted and tweet identifier to log.txt
-            log_file = open("/home/pi/Sync/twitter/Mining_Log.txt", 'a')
+            log_file = open('pathname for mining_log.txt', 'a')
             
             self.save_tweet = 0
             
-            # Checks if user has a location, then if location is in UK list
+            # Checks if user has a location, then if location matches reqs
             try:
                 if status.user.location is not None: 
-                    if (sum([1 for loc in uk_places if loc in status.user.location.lower()]) > 0) and (sum([1 for place in no_places if place in status.user.location]) == 0):
+                    if (sum([1 for loc in incl_places if loc in status.user.location.lower()]) > 0) and (sum([1 for place in excl_places if place in status.user.location]) == 0):
                         self.save_tweet = 1
             except:
                 pass
@@ -72,37 +72,25 @@ class MyStreamListener(StreamListener):
                 try:
                     if not hasattr(status, 'retweeted_status'):
                         try:
-                            if any(status.extended_tweet["full_text"].find(keyword) > -1 for keyword in keywords):
-                                twt_creat = status.created_at
-                                user_data.append(twt_creat)
+                            if any(status.extended_tweet['full_text'].find(keyword) > -1 for keyword in keywords):
+                                twt = user_data.append(status.extended_tweet['full_text'])
+                            else AttributeError:
+                                twt = user_data.append(status.text)
+                                
+                            twt_creat = status.created_at
+                            user_data.append(twt_creat)
 
-                                sn = status.user.screen_name
-                                user_data.append(sn)
+                            sn = status.user.screen_name
+                            user_data.append(sn)
                                 
-                                user_data.append(status.user.location)
-                                
-                                user_data.append(status.extended_tweet["full_text"])
+                            user_data.append(status.user.location)
+                            user_data.append(twt)
                             
-                                log_file.write(f"{datetime.datetime.now()},{sn},{twt_creat}\n")
-                                log_file.close()
+                            log_file.write(f'{datetime.datetime.now()}, {twt_creat}\n')
+                            log_file.close()
 
-                        except AttributeError:
-                            if any(status.text.find(keyword) > -1 for keyword in keywords):
-                                twt_creat = status.created_at
-                                user_data.append(twt_creat)
+                        append_list_as_row('dataset.csv', user_data)
 
-                                sn = status.user.screen_name
-                                user_data.append(sn)
-                                
-                                user_data.append(status.user.location)
-                                
-                                user_data.append(status.text)
-                                
-                                log_file.write(f"{datetime.datetime.now()},{sn},{twt_creat}\n")
-                                log_file.close()
-
-                        append_list_as_row('Dataset.csv', user_data)
-                       
                 except:
                     pass
 
@@ -111,7 +99,7 @@ class MyStreamListener(StreamListener):
         
 myStream = tweepy.Stream(auth=api.auth, listener=MyStreamListener(), tweet_mode='extended')
 
-myStream.filter(track=keywords, languages=["en"])
+myStream.filter(track=keywords, languages=['en'])
 
 
 
